bf966cf9b373c84fd973401cf2f190cc98806c3e
yuriy <wm.info@bk.ru>
2020-12-31 00:01:38 +0500

    Create Bird.java

---
+++ offline/animals/Bird.java
@@ -0,0 +1,12 @@
+package lesson05.offline.animals;
+
+import lesson05.offline.models.Animal;
+
+public class Bird extends Animal {
+
+    public Bird(String name, float maxRun, float maxSwim, float maxJump) {
+        super(name, maxRun, maxSwim, maxJump);
+    }
+
+}
+
