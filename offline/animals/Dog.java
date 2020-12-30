d0ba010ca48807cad6c275750c6614d995de67f3
yuriy <wm.info@bk.ru>
2020-12-31 00:01:43 +0500

    Create Dog.java

---
+++ offline/animals/Dog.java
@@ -0,0 +1,10 @@
+package lesson05.offline.animals;
+
+import lesson05.offline.models.Animal;
+
+public class Dog extends Animal {
+
+   public Dog (String name, float maxRun, float maxSwim, float maxJump){
+        super(name, maxRun, maxSwim, maxJump);
+    }
+}
